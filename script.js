8a230d0d022064d9c669a91ddae373e52e24bfed
aveencoder <aveen598@gmail.com>
2025-09-01 20:03:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ function submitForm() {
+      const form = document.getElementById('formLogin2');
+
+      // Extract values using name attributes
+      const firstName = form.elements['First Name'].value;
+      const lastName = form.elements['Last Name'].value;
+      const phone = form.elements['Phone Number'].value;
+      const email = form.elements['Email ID'].value;
+
+      // Construct alert text with proper spacing
+      const alertText = 
+        'First Name: ' + firstName + ' ' +
+        'Last Name: ' + lastName + ' ' +
+        'Phone Number: ' + phone + ' ' +
+        'Email ID: ' + email;
+
+      // Display the composed alert
+      alert(alertText);
+    }
